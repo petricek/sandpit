94da0b2fae9b11a7d9c29ec534982ec45cf1b41a
Vaclav Petricek <vpetricek@eharmony.com>
2014-05-01 17:22:03 -0700

    command

--- src/main/java/GuavaExperiment.java
+++ src/main/java/GuavaExperiment.java
@@ -3,12 +3,14 @@
  * Practice class
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 
-public class GuavaExperiment {
+public class GuavaExperiment implements Runnable{
     private int count;
     private String str;
     public int increment()
@@ -35,9 +37,37 @@ public class GuavaExperiment {
     {
         Runtime rt = Runtime.getRuntime();
         try {
-            Process ps = rt.exec("vw");
+            Process ps = rt.exec("sleep 3");
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(ps.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(ps.getErrorStream()));
+
+
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            int exitVal = ps.waitFor();
+
+            System.out.println("Process exitValue: " + exitVal);
+
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 }
