3370df30dd8689a320934a2af976f047cd9248d4
Vaclav Petricek <v.petricek@gmail.com>
2014-05-01 22:03:47 -0700

    disable voldy

--- src/test/java/GuavaExperimentTest.java
+++ src/test/java/GuavaExperimentTest.java
@@ -2,11 +2,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Assert;
 import org.junit.Test;
-import voldemort.client.ClientConfig;
-import voldemort.client.SocketStoreClientFactory;
-import voldemort.client.StoreClient;
-import voldemort.client.StoreClientFactory;
-import voldemort.versioning.Versioned;
+//import voldemort.client.ClientConfig;
+//import voldemort.client.SocketStoreClientFactory;
+//import voldemort.client.StoreClient;
+//import voldemort.client.StoreClientFactory;
+//import voldemort.versioning.Versioned;
 
 
 /**
@@ -59,20 +59,20 @@ public class GuavaExperimentTest {
         tester.run();
     }
 
-    @Test
-    public void testVoldy()
-    {
-        String bootstrapUrl = "tcp://localhost:6666";
-        StoreClientFactory factory = new SocketStoreClientFactory(new ClientConfig().setBootstrapUrls(bootstrapUrl));
-
-        // create a client that executes operations on a single store
-        StoreClient client = factory.getStoreClient("test");
-
-        // do some random pointless operations
-        Versioned<String> value = client.get("hey");
-        client.put("some_key", "wohoo");
-
-//        Assert.assertEquals(tester.addPositive(-5),5);
-    }
+//    @Test
+//    public void testVoldy()
+//    {
+//        String bootstrapUrl = "tcp://localhost:6666";
+//        StoreClientFactory factory = new SocketStoreClientFactory(new ClientConfig().setBootstrapUrls(bootstrapUrl));
+//
+//        // create a client that executes operations on a single store
+//        StoreClient client = factory.getStoreClient("test");
+//
+//        // do some random pointless operations
+//        Versioned<String> value = client.get("hey");
+//        client.put("some_key", "wohoo");
+//
+////        Assert.assertEquals(tester.addPositive(-5),5);
+//    }
 
 }
