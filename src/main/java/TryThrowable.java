2f530f9551ca7690b4b63d6c5839bc334c52e849
Vaclav Petricek <vpetricek@eharmony.com>
2014-05-02 21:13:36 -0700

    Throwable

--- src/main/java/TryThrowable.java
+++ src/main/java/TryThrowable.java
@@ -1,9 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /**
  * Created by petricek on 5/1/14.
  */
 public class TryThrowable {
-    public static void main(String args[]){
+    public static void main(String args[]) throws FileNotFoundException{
 
+        try{
+            FileInputStream file = new FileInputStream("xxx");
+        }
+        catch(Throwable t){
+            t.printStackTrace();
+        }
 
     }
 }
